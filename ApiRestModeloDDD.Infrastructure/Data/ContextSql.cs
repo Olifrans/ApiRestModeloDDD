c3fa79ec43f929235167dcdefef9e78c02a253a6
FRANCISCO OLIVEIRA <fransoliveira@gmail.com>
2021-10-13 22:19:09 -0300

    Context

--- ApiRestModeloDDD.Infrastructure/Data/ContextSql.cs
+++ ApiRestModeloDDD.Infrastructure/Data/ContextSql.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ApiRestModeloDDD.Domain.Entitys;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +10,25 @@ namespace ApiRestModeloDDD.Infrastructure.Data
 {
     public class ContextSql : DbContext
     {
-        
+        public ContextSql()
+        {                
+        }
+
+        public ContextSql(DbContextOptions<ContextSql> options) : base(options)
+        {
+        }
+
+        public DbSet<Cliente> Cliente { get; set; }
+
+        public DbSet<Produto> Produto { get; set; }
+
+        public override SaveChanges()
+        {
+            foreach (var item in collection)
+            {
+
+            }
+            return SaveChanges();
+        }
     }
 }
